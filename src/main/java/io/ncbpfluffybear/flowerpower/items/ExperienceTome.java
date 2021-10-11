c59bfba4d62c7b0ec5bf2848a10ecd84f8eb499f
FluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2021-10-11 12:41:34 -0500

    Fixed xp dupe

--- src/main/java/io/ncbpfluffybear/flowerpower/items/ExperienceTome.java
+++ src/main/java/io/ncbpfluffybear/flowerpower/items/ExperienceTome.java
@@ -91,7 +91,7 @@ public class ExperienceTome extends SlimefunItem implements Listener {
             if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
                 transferExp = Utils.getTotalExperience(p);
             } else {
-                transferExp = EXP_TRANSFER_RATE;
+                transferExp = Math.min(Utils.getTotalExperience(p), EXP_TRANSFER_RATE);
             }
 
             // If overflow, decrease to fill tome

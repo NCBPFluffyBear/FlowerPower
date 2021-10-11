f95465cb36920cd2913e092270a0ff1bebd11073
FluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2021-10-11 12:55:13 -0500

    Delete unnecessary imports

--- src/main/java/io/ncbpfluffybear/flowerpower/items/ExperienceTome.java
+++ src/main/java/io/ncbpfluffybear/flowerpower/items/ExperienceTome.java
@@ -1,7 +1,6 @@
 package io.ncbpfluffybear.flowerpower.items;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
